226a17cba2e742e35f63dade23152338e18fcdf8
Abdullah Khatib <khatibabdullah25@gmail.com>
2026-08-23 18:29:43 -0400

    Saving my current partial initialization of registers so I don't lose my progress.

--- src/main.c
+++ src/main.c
@@ -1,19 +1,65 @@
 #include <stdint.h>
 
 typedef struct {
-  volatile uint32_t GPIOx_MODER;
-  volatile uint32_t GPIOx_OTYPER;
-  volatile uint32_t GPIOx_OSPEEDR;
-  volatile uint32_t GPIOx_PUPDR;
-  volatile uint32_t GPIOx_IDR;
-  volatile uint32_t GPIOx_ODR;
-  volatile uint32_t GPIOx_BSRR;
-  volatile uint32_t GPIOx_LCKR;
-  volatile uint32_t GPIOx_AFRH;
-  volatile uint32_t GPIOx_AFRL;
+  uint32_t GPIOx_MODER;
+  uint32_t GPIOx_OTYPER;
+  uint32_t GPIOx_OSPEEDR;
+  uint32_t GPIOx_PUPDR;
+  uint32_t GPIOx_IDR;
+  uint32_t GPIOx_ODR;
+  uint32_t GPIOx_BSRR;
+  uint32_t GPIOx_LCKR;
+  uint32_t GPIOx_AFRL;
+  uint32_t GPIOx_AFRH;
 } GPIO_Struct;
 
+//Probably not this, because it's GPIOB, but I'll check
+#define GPIOB ((volatile GPIO_Struct *) 40020400)
+
+typedef struct {
+  uint32_t I2C_CR1;
+  uint32_t I2C_CR2;
+  uint32_t I2C_OAR1;
+  uint32_t I2C_OAR2;
+  uint32_t I2C_DR;
+  uint32_t I2C_SR1;
+  uint32_t I2C_SR2;
+  uint32_t I2C_CCR;
+  uint32_t I2C_TRISE;
+  uint32_t I2C_FLTR;
+} I2C_Struct;
+
+#define I2C1 ((volatile I2C_Struct *) 0x40005400)
 
 int main (void) {
+
+  //Configuring the GPIOB Pins 5, 6, and 7, because those are the I2C AF ones
+  
+  //Reset the pins first to 00 each
+  GPIOB->GPIOx_MODER &= ~((3 << 10) | (3 << 12) | (3 << 14));
+
+  //Now set them to AF here
+  GPIOB->GPIOx_MODER |= ((2 << 10) | (2 << 12) | (2 << 14));
+
+  //Set to Open Drain
+  GPIOB->GPIOx_OTYPER |= (1 << 5);
+  GPIOB->GPIOx_OTYPER |= (1 << 6);
+  GPIOB->GPIOx_OTYPER |= (1 << 7);
+
+  //Output speed register, control the output speed
+
+
+
+  //Starting the I2C
+  //enable peripheral
+  I2C1->I2C_CR1 |= (1 << 0);
+  //set start condition to go into controller mode
+  I2C1->I2C_CR1 |= (1 << 8);
+
+
+
+  //Start condition to get into controller mode
+
+
 
 }
